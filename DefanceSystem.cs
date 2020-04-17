7de5ebd6cfd10dcdb2dfec0c2932e6c11d495f5b
İbrahim karabay <46526531+karabayibrahim@users.noreply.github.com>
2020-04-17 04:14:38 +0300

    Add files via upload

---
+++ DefanceSystem.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DitzeGames.MobileJoystick;
+
+public class DefanceSystem : MonoBehaviour
+{
+    public Joystick JoyDefence;
+    public GameObject AtakJoy;
+    public GameObject DefanceJoy;
+    public GameObject Defance;
+    public GameObject DefanceButton;
+    public GameObject AtakHandle;
+    public GameObject Player;
+    public float DefanceTime = 5f;
+    bool ZamanKontrol = false;
+
+    Vector3 Vec2;
+    void Start()
+    {
+        DefanceButton = GameObject.Find("DefanceButton");
+        AtakJoy = GameObject.Find("JoyAtak");
+        Player = GameObject.FindGameObjectWithTag("Player");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(DefanceTime);
+        //Debug.Log(JoyDefence.AxisNormalized.x);
+        Orbit();
+        DefanceAktif();
+        DefanceOrbit();
+        Defanceİnaktif();
+    }
+    void DefanceAktif()
+    {
+        if (DefanceButton.GetComponent<Button>().Pressed)
+        {
+            ZamanKontrol = true;
+            FindObjectOfType<AttackZone>().enabled = false;
+            FindObjectOfType<JoyPlayerMovement>().Anim.SetBool("Defance", true);
+            DefanceJoy.SetActive(true);
+            AtakJoy.SetActive(false);
+            Defance.SetActive(true);
+
+            
+
+
+        }
+        
+
+    }
+    void DefanceOrbit()
+    {
+        if (JoyDefence.AxisNormalized.x != 0 || JoyDefence.AxisNormalized.y != 0)
+        {
+            Debug.Log("Çalışıyor");
+            Player.transform.LookAt(Vec2);
+        }
+
+    }
+    void Orbit()
+    {
+        Vec2 = new Vector3(JoyDefence.AxisNormalized.x * -1*90, 0, JoyDefence.AxisNormalized.y * -1*90);
+        transform.LookAt(Vec2);
+
+    }
+    void Defanceİnaktif()
+    {
+        if (ZamanKontrol)
+        {
+            DefanceTime -=Time.deltaTime;
+        }
+        if (DefanceTime <= 0)
+        {
+            FindObjectOfType<AttackZone>().enabled = true;
+            FindObjectOfType<JoyPlayerMovement>().Anim.SetBool("Defance", false);
+            DefanceJoy.SetActive(false);
+            AtakJoy.SetActive(true);
+            Defance.SetActive(false);
+            DefanceTime = 5f;
+            ZamanKontrol = false;
+        }
+    }
+   
+}
---
+++ RadialProg.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RadialProg : MonoBehaviour
+{
+    public float maximum;
+    public float minimum;
+    public float current;
+    public Image mask;
+    public Image fill;
+    // Start is called before the first frame update
+    void Start()
+    {
+        maximum = FindObjectOfType<DefanceSystem>().DefanceTime;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        current = FindObjectOfType<DefanceSystem>().DefanceTime;
+
+        GetCurrentFill();
+    }
+    void GetCurrentFill()
+    {
+        fill.fillAmount = current/maximum;
+        
+    }
+}
